ffbf62764dcb3a57a6ea9bb2428e3eca7742eeed
Light2233 <74866276+Light2233@users.noreply.github.com>
2021-01-26 19:09:47 +0300

    add

--- site qr/java.js
+++ site qr/java.js
@@ -1,32 +1,3 @@
-var qrcode = new QRCode("qrcode");
-
-function makeCode () {    
-  var elText = document.getElementById("text");
-  
-  if (!elText.value) {
-    alert("Input a text");
-    elText.focus();
-    return;
-  }
-  let number = Math.ceil(Math.random() * 100);
-
-
-
-alert(number);
-  qrcode.makeCode(elText.value);
-}
-
-makeCode();
-
-$("#text").
-  on("blur", function () {
-    makeCode();
-  }).
-  on("keydown", function (e) {
-    if (e.keyCode == 13) {
-      makeCode();
-    }
-  });
 
 
 
